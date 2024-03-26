ef49abc7d99ecaec16275ee423aac7a7ca78dcff
ringeringeraja <joaosan177@gmail.com>
2024-03-26 17:26:43 -0300

    update

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,49 +1,17 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
-import { watch, provide } from 'vue'
-import {
-  useStore,
-  meta,
-  createGlobalStateManager,
-  createI18n,
-  GLOBAL_STATE_KEY,
-  I18N_KEY,
-  type StoreContext,
-
-} from 'aeria-ui'
-
 import '@aeria-ui/ui/style.css'
 import '../../src/style/main.less'
 
-
-const globalStateManager = createGlobalStateManager()
-const i18n = createI18n()
-
-provide(GLOBAL_STATE_KEY, globalStateManager.__globalState)
-provide(I18N_KEY, i18n.__globalI18n)
-
-const context: StoreContext = {
-  i18n: i18n.__globalI18n,
-  manager: globalStateManager,
-}
-
 const { isDark } = useData()
-
-const metaStore = meta(context)
-
-watch(isDark, (value) => {
-  metaStore.theme = value
-    ? 'dark'
-    : 'default'
-})
 </script>
 
 <template>
   <div
     id="aeria-layout"
     :class="
-      metaStore.theme === 'dark'
+      isDark
         ? 'main--dark'
         : 'main'
     "
