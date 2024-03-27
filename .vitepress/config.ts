f3f9d2fd327d2762a8ae468a93257daa9c27a05b
ringeringeraja <joaosan177@gmail.com>
2024-03-27 02:36:45 -0300

    update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -20,9 +20,7 @@ export default defineConfig({
       include: ['bson'],
     },
     plugins: [
-      aeriaIcons({
-        hash: true,
-      }),
+      aeriaIcons(),
     ],
   },
   themeConfig: {
